be12bddc32acd6d95dacdf8579944498f8cd0482
pouria-drd <pouriadrd@gmail.com>
2026-07-03 18:10:16 +0330

    added product models

--- shop/models/__init__.py
+++ shop/models/__init__.py
@@ -1,11 +1,1 @@
-from .feature import FeatureModel
-from .product import ProductModel
-from .product_plan import ProductPlanModel
-from .plan_feature import PlanFeatureModel
-
-__all__ = [
-    "FeatureModel",
-    "ProductModel",
-    "ProductPlanModel",
-    "PlanFeatureModel",
-]
+from .product import *
