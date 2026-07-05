d8665688445a98eb8df8be10ad0d7eeffb60bf70
pouria-drd <pouriadrd@gmail.com>
2026-07-05 21:13:08 +0330

    added more details to vpn servicee serializer

--- shop/api/serializers/vpn_service/vpn.py
+++ shop/api/serializers/vpn_service/vpn.py
@@ -8,14 +8,24 @@ class VPNSerializer(serializers.ModelSerializer):
     VPN service serializer.
     """
 
+    product_title = serializers.CharField(
+        source="order_item.product.title",
+        read_only=True,
+    )
+    plan_title = serializers.CharField(
+        source="order_item.plan.title",
+        read_only=True,
+    )
+
     class Meta:
         model = VPNProvisionedServiceModel
         fields = (
             "id",
+            "product_title",
+            "plan_title",
             "subscription_link",
             "content",
             "expires_at",
             "created_at",
-            "updated_at",
         )
-        read_only_fields = ["__all__"]
+        read_only_fields = fields
